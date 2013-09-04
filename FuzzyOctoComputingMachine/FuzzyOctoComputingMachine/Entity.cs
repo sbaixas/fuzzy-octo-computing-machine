ea0649d982944e47520b392e73780a11d7af0218
Matías Valenzuela Fuentes <mvalenzuela13@miuandes.cl>
2013-09-03 23:20:53 -0400

    Animal/Entity 1.1

---
+++ FuzzyOctoComputingMachine/FuzzyOctoComputingMachine/Entity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuzzyOctoComputingMachine
+{
+    class Entity
+    {
+        int y;
+        int x;
+        int id;
+
+        public Entity(int x, int y, int id)
+        {
+            this.x = x;
+            this.y = y;
+            this.id = id;
+        }
+        public int Gety()
+        {
+            return y;
+        }
+        public int Getx()
+        {
+            return x;
+        }
+        public int GetID()
+        {
+            return id;
+        }
+    }
+}
---
+++ FuzzyOctoComputingMachine/FuzzyOctoComputingMachine/animal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuzzyOctoComputingMachine
+{
+    class animal : Entity
+    {
+        string name;
+        int age;
+
+        public animal(string name, int age, int x, int y, int id)
+            : base(x, y, id)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+        public int GetAge()
+        {
+            return age;
+        }
+        public void ChangeAge(int a)
+        {
+            age = a;
+        }
+    }
+}
